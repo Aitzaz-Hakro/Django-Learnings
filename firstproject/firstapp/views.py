d24a41de613f41bbcc0c489ba7f003a826c9c1fb
Aitzaz Hassan <aitzazhakro123@gmail.com>
2026-06-12 21:20:31 +0500

    day1: completed

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import AdmissionForm
 
 # Create your views here.
 def hello_world(request):
@@ -9,3 +10,15 @@ def hello_world(request):
 class HelloPakistan(View):
     def get(self, request):
         return HttpResponse("Hello Pakistanis")
+
+def home(request):
+    form = AdmissionForm()
+
+    if request.method == 'POST':
+        form = AdmissionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Admission successful")
+
+    return render(request, 'index.html', {'form': form})
+
